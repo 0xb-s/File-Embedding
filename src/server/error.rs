2c0ef42d6a01ed9be8d8553fc4cfeed98d03ecee
0xb-s <145866191+0xb-s@users.noreply.github.com>
2023-09-29 10:17:14 -0700

    Update error.rs

--- src/server/error.rs
+++ src/server/error.rs
@@ -1,1 +1,11 @@
 
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum MyError {
+    CreateTableError(String),
+    EmbedFileError(String),
+    EmbedDirectoryError(String),
+    // ... todo
+}
