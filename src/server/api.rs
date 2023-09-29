816e1b0ab5f0ebcdc3e7ecd39cedf68411fb0e31
0xb-s <145866191+0xb-s@users.noreply.github.com>
2023-09-29 10:18:03 -0700

    Update api.rs

--- src/server/api.rs
+++ src/server/api.rs
@@ -1,1 +1,18 @@
+use crate::db::DbForStorageFile;
+use crate::read_path::File;
 
+use axum::response::IntoResponse;
+
+use axum::{extract::Json, response::Json as JsonResponse};
+use serde::Deserialize;
+use sqlx::SqlitePool;
+
+use std::path::Path;
+use std::sync::Arc;
+#[derive(Deserialize)]
+pub struct EmbedReq {
+    pub path: String,
+}
+pub struct AppState {
+    pub pool: Arc<SqlitePool>,
+}
