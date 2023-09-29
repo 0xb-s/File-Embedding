4e24615a89bfab95ce9e8399febb33904823fa10
0xb-s <145866191+0xb-s@users.noreply.github.com>
2023-09-29 10:18:50 -0700

    Update api.rs

--- src/server/api.rs
+++ src/server/api.rs
@@ -16,3 +16,37 @@ pub struct EmbedReq {
 pub struct AppState {
     pub pool: Arc<SqlitePool>,
 }
+
+pub async fn create_db_table(state: Arc<AppState>) -> impl IntoResponse {
+    DbForStorageFile::create_table(&state.pool).await;
+    JsonResponse(serde_json::json!({"status": "ok"}))
+}
+
+pub async fn file_post(Json(req): Json<EmbedReq>, state: Arc<AppState>) -> impl IntoResponse {
+    let path = Path::new(&req.path);
+    let file = File::new(path.to_path_buf());
+    DbForStorageFile::embed_file(&state.pool, &file).await;
+    Json(serde_json::json!({"status": "ok"}))
+}
+
+pub async fn directory_post(Json(req): Json<EmbedReq>, state: Arc<AppState>) -> impl IntoResponse {
+    let path = Path::new(&req.path);
+    DbForStorageFile::embed_directory(&state.pool, &path)
+        .await
+        .unwrap();
+    Json(serde_json::json!({"status": "ok"}))
+}
+// pub async fn test(
+//     pool: Arc<SqlitePool>,
+//     Json(EmbedReq { path }): Json<EmbedReq>,
+// ) -> impl axum::response::IntoResponse {
+//     let path = Path::new(&path);
+
+//     match DbForStorageFile::embed_directory(&pool, &path).await {
+//         Ok(_) => JsonResponse(serde_json::json!({"status": "ok"})),
+//         Err(err) => {
+//             eprintln!("Error embedding directory: {}", err);
+//             JsonResponse(serde_json::json!({"status": "error", "error": err.to_string()}))
+//         }
+//     }
+// }
