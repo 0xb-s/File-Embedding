c4b6e840f26eba63e14d4257005324fdf5f79fe2
0xb-s <145866191+0xb-s@users.noreply.github.com>
2023-09-29 10:19:11 -0700

    Update read_path.rs

--- src/read_path.rs
+++ src/read_path.rs
@@ -1,1 +1,16 @@
 
+use std::path::Path;
+use std::path::PathBuf;
+
+pub struct File {
+    pub path: PathBuf,
+}
+
+impl File {
+    pub fn new(path: PathBuf) -> Self {
+        Self { path: path }
+    }
+    pub fn read_directory<P: AsRef<Path>>(path: P) {
+        std::fs::read_dir(path).unwrap();
+    }
+}
